fbe1f53de9551601f9d3dc6c0e00c604350d37e1
GabbyGreat <gabrieloranekwu@gmail.com>
2023-03-26 11:42:00 +0100

    fixed the label:

--- lib/Sensitive_contentDetector_func/Nudity_imagecheck.dart
+++ lib/Sensitive_contentDetector_func/Nudity_imagecheck.dart
@@ -1,25 +1,22 @@
-
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
-
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 
 Future<bool> detectNudityInImage(File imageFile) async {
   /*The below function is made to use the check for image but due to some unstable and current change in the firebase_ml_vision
   it was giving error*/
-  final  visionImage = FirebaseVisionImage.fromFile(imageFile);
-  final ImageLabeler labeler = FirebaseVision.instance.imageLabeler();
+
+  final visionImage = InputImage.fromFile(imageFile);
+  final ImageLabelerOptions options =
+      ImageLabelerOptions(confidenceThreshold: 0.5);
+  final labeler = ImageLabeler(options: options);
   final List<ImageLabel> labels = await labeler.processImage(visionImage);
 
   for (ImageLabel label in labels) {
-    if (label.text?.toLowerCase() == 'nudity' && label.confidence != null &&
-        label.confidence! > 0.5) {
+    if (label.label == 'nudity' && label.confidence > 0.5) {
       return true;
     }
   }
+
   return false;
 }
-
-
-
-
